9a64641e3a971332764efb1355d83e29fe4e22fb
dykai <yingkaidang@gmail.com>
2020-10-12 00:02:36 +0800

    build.js add --tb option

--- config/build.js
+++ config/build.js
@@ -1,20 +1,10 @@
 #! /bin/env node
-const { spawn, spawnSync } = require('child_process')
-const { shell } = require('electron')
+const {shell_do} = require("./utils")
+const yargs = require("yargs")
 
-function shell_do(cmd, conf) {
-    conf = conf || {}
-    const env = conf.env || process.env
-
-    let p = spawnSync(cmd, {
-        shell: true,
-        stdio: 'inherit',
-        env
-    })
-    if (p.status !== 0) {
-        process.exit(p.status)
-    }
-}
+const {tb} = yargs.version(false).strict().option('tb', {
+    description: 'use taobao cdn while build electron'
+}).boolean('tb').argv
 
 console.log('[Webpack] compiling for main...')
 shell_do('webpack --config ./config/webpack.prod.main.config.js')
@@ -22,10 +12,14 @@ shell_do('webpack --config ./config/webpack.prod.main.config.js')
 console.log('[Webpack] compiling for renderer...')
 shell_do('webpack --config ./config/webpack.prod.renderer.config.js')
 
-console.log('[ElectronBuilder] building electron...')
-shell_do('electron-builder', {
-    env: Object.assign({}, process.env, {
-        ELECTRON_MIRROR: "https://cdn.npm.taobao.org/dist/electron/",
-        ELECTRON_BUILDER_BINARIES_MIRROR: "https://npm.taobao.org/mirrors/electron-builder-binaries/"
+console.log(`[ElectronBuilder] building electron...${tb ? " [tb cdn]" : ""}`)
+if (tb) {
+    shell_do('electron-builder', {
+        env: Object.assign({}, process.env, {
+            ELECTRON_MIRROR: "https://cdn.npm.taobao.org/dist/electron/",
+            ELECTRON_BUILDER_BINARIES_MIRROR: "https://npm.taobao.org/mirrors/electron-builder-binaries/"
+        })
     })
-})
+} else {
+    shell_do('electron-builder')
+}
--- config/utils.js
+++ config/utils.js
@@ -1,9 +1,25 @@
 const path = require('path')
+const {spawnSync} = require('child_process')
 
 function abs_path(p) {
     return path.resolve(__dirname, '..', p)
 }
 
+function shell_do(cmd, conf) {
+    conf = conf || {}
+    const env = conf.env || process.env
+
+    let p = spawnSync(cmd, {
+        shell: true,
+        stdio: 'inherit',
+        env
+    })
+    if (p.status !== 0) {
+        process.exit(p.status)
+    }
+}
+
 module.exports = {
-    abs_path
+    abs_path,
+    shell_do
 }
